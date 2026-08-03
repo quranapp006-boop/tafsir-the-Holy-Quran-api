1197b0d3887af8277163529bfc9813fc9386d73a
quranapp006-boop <quranapp006@gmail.com>
2026-08-03 22:46:39 +0100

    v2

--- dist/types.js
+++
@@ -1,6 +0,0 @@
-/**
- * Type definitions for the Adhkar (Islamic supplications) dataset and API.
- * These types describe both the on-disk JSON shape and the API response shapes.
- */
-export {};
-//# sourceMappingURL=types.js.map
